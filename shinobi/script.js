7ab48eed49a59d75609c169dfc009fffbdb064ab
Khairin Kamarizal <74864017+khairinkamarizal@users.noreply.github.com>
2022-07-10 00:09:39 +0800

    Add files via upload

--- shinobi/script.js
+++ shinobi/script.js
@@ -198,3 +198,9 @@ $('.slider').each(function() {
   
   advance();
 });
+
+$(window).on('load', function() { // makes sure the whole site is loaded 
+  $('#status').fadeOut(); // will first fade out the loading animation 
+  $('#preloader').delay(1500).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(350).css({'overflow':'visible'});
+})
